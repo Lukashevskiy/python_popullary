d3822961243f9b8c931bdcb86a0811b32fe731c3
dmitriy <dim4ik-super-klass@mail.ru>
2023-01-27 17:19:29 +0800

    change structure

--- Tic_Tac_Toe/main.py
+++ Tic_Tac_Toe/main.py
@@ -31,46 +31,66 @@ def is_field_filled(field):
                 return False
     return True
 
+def is_cell_valid(field, n, x, y):
+    if 0 > x or x >= n:
+        return False
+
+    if 0 > y or y >= n:
+        return False
+        
+    if field[y][x] != ' ':
+        return False
+
+    return True    
+
+def check_row(field, n, y, current_player):
+    for x in range(0, n):
+        if field[y][x] != current_player:
+            return False
+
+    return True    
+
+def check_column(field, n, x, current_player):
+    return True
+
+def check_diag(field, n, diag_i, current_player):
+    return True
+
+def is_win(field, n, current_player):
+    for y in range(0, n):
+        if check_row(field, y, current_player):
+            return True
+
+    return False
+
 n = 3
-field = list()
-for i in range(n):
-    line = list()
-    for j in range(n):
-        line.append(' ')
-    field.append(line)
+field = generate_field(n)
 current_player = 'X'
 step_player = []
 
 
+
 while True:
     print('///'*10)
-    
-    for i, line in enumerate(field):
-        print(*line,sep='|')
-        if i < n - 1:
-            print('-' * (n * 2 -1))
-    print("Сейчас ходит - ", current_player)
-    y, x = map(int,input('Введите координаты: ').split())
-    if 0 <= x < n and 0 <= y < n:
-        if field[y][x] == ' ':
-            field[y][x] = current_player
-            if current_player == 'X':
-                current_player = '0'
-            else:
-                current_player = 'X'
-        else:
-            print("Ячейка уже занята!")
+    print_field(field, n)
+    step_player = get_player_step(current_player)
+    y, x = step_player[0], step_player[1]
+
+    if is_cell_valid(field, n, y, x):
+        field[y][x] = current_player
     else:
-        print("Не корректный ввод")
-    filled_cells = 0
-    for line in field:
-        for el in line:
-            if el != ' ':
-                filled_cells += 1
-    if filled_cells == n*n:
+        print("Invalid cell")
+        continue
+   
+    if is_win(field, n, current_player):
+        print("Win -", current_player)
+        break 
+
+    if is_field_filled(field):
         print('Конец игры.')
         break
+
     print('///'*10)
 
 print('///'*10)
-# print_field(field)
+print_field(field)
