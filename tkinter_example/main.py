a1a73c77a44c8c85d0d0a47dd239ea8a501ed3d8
dmitriy <dim4ik-super-klass@mail.ru>
2023-02-07 16:59:13 +0800

    add examples

---
+++ tkinter_example/main.py
@@ -0,0 +1,17 @@
+from tkinter import Tk, Label, Button
+# import tkinter
+
+window = Tk()
+
+window.title("Hello World")
+window.geometry("640x480")
+
+def add_label():
+    Label(window, text="Hello World").pack()
+
+button = Button(window, 
+                text="Пам пам", 
+                command=add_label)
+
+button.pack()
+window.mainloop()
